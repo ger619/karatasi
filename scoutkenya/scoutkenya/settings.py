699d785df19e1f26f2190ac321b4dd1a1df41b4f
David Ger <draker74@gmail.com>
2019-03-26 10:50:39 +0300

    from mac

--- scoutkenya/scoutkenya/settings.py
+++ scoutkenya/scoutkenya/settings.py
@@ -32,12 +32,15 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'karatasi.apps.KaratasiConfig',
+    'users.apps.UsersConfig',
+#    'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
 ]
 
 MIDDLEWARE = [
@@ -119,3 +122,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
 STATIC_URL = '/static/'
+
+#CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+LOGIN_REDIRECT_URL = 'karatasi-home'
+
+LOGIN_URL = 'login'
