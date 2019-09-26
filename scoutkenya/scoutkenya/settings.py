5fa4ec21b321fc84f4aa98348644b4d583db5697
David Ger <draker74@gmail.com>
2019-09-26 16:03:25 +0300

    no message

--- scoutkenya/scoutkenya/settings.py
+++ scoutkenya/scoutkenya/settings.py
@@ -124,6 +124,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/dev/howto/static-files/
 
+MEDIA_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
